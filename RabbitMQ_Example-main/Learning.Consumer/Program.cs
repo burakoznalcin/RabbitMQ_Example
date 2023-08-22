b9194ce0ae33ac2d7c8ab05a4c6281966176b311
Burak Öznalçın <burakoznalcn@icloud.com>
2023-08-22 18:13:29 +0300

    RabbitMQ Example

---
+++ RabbitMQ_Example-main/Learning.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqp://guest:guest@localhost:5672");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(queue: "example-queue", false, consumer);
+
+
+consumer.Received += (sender, e) =>
+{
+
+    Console.WriteLine($"Kuyruk mesaj  byte verisi {Encoding.UTF8.GetString(e.Body.Span)}");
+
+};
+
+Console.Read();
---
+++ RabbitMQ_Example-main/Learning.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqp://guest:guest@localhost:5672");
+
+
+using IConnection connection = factory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare("example-queue",exclusive:false);
+
+//byte[] message =  Encoding.UTF8.GetBytes("Merhaba");
+
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+await Task.Delay(3000);
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+    await Task.Delay(1000);
+}
+
+
+
+Console.Read();
